57946d039ecbcfb02f9d39a25e1e37593f97d6d6
Yu Te <yu@macpcx.com>
2018-01-28 12:47:11 -0800

    add mines

--- Assets/Mine.cs
+++ Assets/Mine.cs
@@ -7,12 +7,29 @@ using UnityEngine;
 
 public class Mine : MonoBehaviour
 {
+	public float ArmTime = 5;
+
 	public Sub Parent { get; set; }
 
 	public GameObject ExplosionPrefab;
 
+	private bool armed;
+
+	public void Start()
+	{
+		StartCoroutine(ArmAfter());
+	}
+
+	private IEnumerator ArmAfter()
+	{
+		yield return new WaitForSeconds(ArmTime);
+		armed = true;
+	}
+		
 	public void OnCollisionEnter(Collision collision)
 	{
+		if (!armed)
+			return;
 		var targetable = collision.collider.GetComponentInParent<TorpedoTargetable>();
 		if (targetable != null)
 			targetable.HitByTorpedo(this);

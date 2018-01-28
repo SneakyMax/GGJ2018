0cfafc068d35a0b654bf86ea70cb2c7860b14001
Yu Te <yu@macpcx.com>
2018-01-28 12:46:44 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Mine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+	public Sub Parent { get; set; }
+
+	public GameObject ExplosionPrefab;
+
+	public void OnCollisionEnter(Collision collision)
+	{
+		var targetable = collision.collider.GetComponentInParent<TorpedoTargetable>();
+		if (targetable != null)
+			targetable.HitByTorpedo(this);
+		Explode();
+	}
+
+	public void Explode()
+	{
+		Instantiate(ExplosionPrefab, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/SubMineController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using XInputDotNetPure;
+
+class SubMineController : MonoBehaviour
+{
+	public float FireInterval = 1;
+
+	public GameObject MinePrefab;
+
+	private float lastFireTime;
+
+	private Sub sub;
+
+	private Transform minePoint;
+
+	public void Awake()
+	{
+		sub = GetComponent<Sub>();
+		minePoint = GetComponentInChildren<MinePoint>().transform;
+	}
+
+	public void Update()
+	{
+		if (sub.IsDestroyed)
+			return;
+
+		if (sub.InputState.Buttons.Y == ButtonState.Pressed && Time.time - lastFireTime > FireInterval)
+		{
+			lastFireTime = Time.time;
+			FireMine();
+		}
+	}
+
+	private void FireMine()
+	{
+		var newMineObj = Instantiate(MinePrefab, minePoint.position, minePoint.rotation);
+		var mine = newMineObj.GetComponent<Mine>();
+		mine.Parent = sub;
+
+		TaggableManager.Instance.TagForAllBut(sub.Taggable, sub.Player, 2);
+	}
+}

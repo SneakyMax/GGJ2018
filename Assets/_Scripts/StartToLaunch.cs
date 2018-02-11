4af413bcdafe50b18908bcbe3b75d7d77900f804
Unknown <maxkbennett@gmail.com>
2018-02-11 15:42:30 -0800

    3:42

---
+++ Assets/_Scripts/StartToLaunch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class StartToLaunch : MonoBehaviour
+{
+
+    float timer; 
+    public float FlashInterval;
+    public int BuildIndex;
+    public MonoBehaviour Target;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(FlashLoop());
+	}
+	
+    private IEnumerator FlashLoop ()
+    {
+        while (true)
+        {
+            Target.enabled = false;
+            yield return new WaitForSeconds(FlashInterval);
+            Target.enabled = true;
+            yield return new WaitForSeconds(FlashInterval);
+
+            
+
+        }
+
+    }
+    public void Update()
+    {
+        var state = GamePad.GetState(PlayerIndex.One);
+        if (state.Buttons.Start == ButtonState.Pressed)
+        {
+            StopCoroutine(FlashLoop());
+            Target.enabled = false;
+        }
+
+        
+    }
+}
---
+++ Assets/_Scripts/TitleFade.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using XInputDotNetPure;
+using UnityEngine.SceneManagement;
+
+public class TitleFade : MonoBehaviour {
+
+    private float timer;
+    private bool canLoad = false;
+    public int FadeTime;
+    public Image image;
+    public int BuildIndex;
+
+    // Use this for initialization
+    void Start () {
+        image = GetComponent<Image>();
+        image.DOFade(0, FadeTime);
+	}
+
+    public void Update()
+    {
+        var state = GamePad.GetState(PlayerIndex.One);
+        if (state.Buttons.Start == ButtonState.Pressed)
+        {
+            image.DOFade(1, FadeTime);
+            canLoad = true;
+ 
+        }
+        if (image.color.a == 1 && canLoad == true)
+        {
+            SceneManager.LoadScene(BuildIndex);
+        }
+
+    }
+
+
+
+
+}

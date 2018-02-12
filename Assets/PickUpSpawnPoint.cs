ed3306284a3142b65ae9cfc0e8797998f458a43d
Unknown <maxkbennett@gmail.com>
2018-02-11 16:34:31 -0800

    4:34

---
+++ Assets/PickUpSpawnPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSpawnPoint : MonoBehaviour
+{
+
+    private bool empty = true;
+    private float timer;
+
+    
+
+    public int TimeToSpawn;
+    public GameObject[] pickups;
+    public GameObject ThisPickup;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (empty == true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= TimeToSpawn)
+            {
+                empty = false;
+                ThisPickup = Instantiate(pickups[Random.Range(0, pickups.Length)], transform.position, Quaternion.identity);
+            }
+        }
+        if (empty == false && ThisPickup == null)
+        {
+            empty = true;
+        }
+	}
+}
